0f752a50a5aeedc42ce5bbca2b14c002dca2bf8e
lindx <linder.igor@gmail.com>
2022-01-17 09:52:35 +0300

    Java non-blocking console input on Linux
    On UNIX systems, the 'stty' command can change modes.
    
    man sh
    
          -c        Read commands from the command_string operand instead of from the
                    standard input. Special parameter 0 will be set from the
                    command_name operand and the positional parameters ($1, $2, etc.)
                    set from the remaining argument operands.
    -------
    
    sh -c "stty raw < /dev/tty"      # Into "raw" mode
    sh -c "stty sane < /dev/tty"     # to "cooked" mode
    
    -------
    
    java:
    
      String[] raw = { "/bin/sh", "-c", "stty raw < /dev/tty" };
      String[] sane = { "/bin/sh", "-c", "stty sane < /dev/tty" };
    
      Runtime.getRuntime().exec(raw).waitFor();  // raw mode on
      Runtime.getRuntime().exec(sane).waitFor();  // switch sane mode

--- src/main/java/lx/lindx/bash/App.java
+++ src/main/java/lx/lindx/bash/App.java
@@ -1,63 +1,44 @@
 package lx.lindx.bash;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.Console;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.Reader;
 
 /**
  * App
  */
 public class App {
 
-  private static String propPathFile = "main.properties";
-  private static Properties properties;
-  private static OutputStream out;
-  private static InputStream in;
+  private static Console console;
+  private static Reader reader;
 
-  private static final Logger LOGGER;
+  public static void main(String[] args) throws InterruptedException, IOException {
 
-  static {
-    properties = new Properties();
-    LOGGER = LogManager.getLogger(App.class.getName());
+    String[] raw = { "/bin/sh", "-c", "stty raw < /dev/tty" };
+    String[] sane = { "/bin/sh", "-c", "stty sane < /dev/tty" };
 
-    try {
-      out = new FileOutputStream(new File("src/main/resources", propPathFile));
-      in = App.class.getClassLoader().getResourceAsStream(propPathFile);
+    Runtime.getRuntime().exec(raw).waitFor();
 
-      if (out == null || in == null) {
-        System.out.println("properties file not found");
-        System.exit(1);
+    console = System.console();
+    reader = console.reader();
+
+    char ch;
+
+    while (true) {
+
+      ch = (char) reader.read();
+
+      if ('q' == ch) {
+        Runtime.getRuntime().exec(sane).waitFor();
+        System.exit(0);
+      } else if ('w' == ch) {
+        System.out.print("orked!");
+      } else if ('\t' == ch) {
+        System.out.println("\n\rsome else \r");
+        System.out.println("some else \r");
+        System.out.println("some else \r");
+        System.out.print("\rlast result");
       }
-
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
     }
-  }
-
-  public static void main(String[] args) throws IOException {
-
-    properties.setProperty("java.vm.name", System.getProperty("java.vm.name"));
-    properties.setProperty("java.home", System.getProperty("java.home"));
-    properties.setProperty("os.version", System.getProperty("os.version"));
-    properties.setProperty("os.name", System.getProperty("os.name"));
-    properties.setProperty("package", App.class.getCanonicalName());
-    properties.store(out, "");
-
-    System.out.println(properties.getProperty("java.vm.name"));
-    System.out.println(properties.getProperty("java.home"));
-    System.out.println(properties.getProperty("os.version"));
-    System.out.println(properties.getProperty("os.name"));
-    System.out.println(properties.getProperty("package"));
-
-    LOGGER.debug("test log debug message", App.class);
-    LOGGER.info("test log info message", App.class);
-    LOGGER.error("test log error message", App.class);
   }
 }
