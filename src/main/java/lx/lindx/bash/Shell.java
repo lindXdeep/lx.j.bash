0e4684b93a018f4593913a757bcdf4eab11faf9b
lindx <linder.igor@gmail.com>
2022-01-19 09:59:16 +0300

    bug fix

--- src/main/java/lx/lindx/bash/Shell.java
+++ src/main/java/lx/lindx/bash/Shell.java
@@ -1,15 +1,14 @@
 package lx.lindx.bash;
 
+import static lx.lindx.bash.util.Util.getLogger;
+
 import java.io.Console;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
 
 import lx.lindx.bash.err.UndefinedKeysException;
 import lx.lindx.bash.sys.Esc;
 import lx.lindx.bash.sys.Tty;
-import static lx.lindx.bash.util.Util.getLogger;
 
 public class Shell implements Runnable {
 
@@ -28,68 +27,63 @@ public class Shell implements Runnable {
 
   @Override
   public void run() {
-    try {
-      mainLoop();
-    } catch (IOException e) {
-      Tty.sane();
-      getLogger().error(e.getMessage());
-    }
-  }
-
-  private void mainLoop() throws IOException {
 
     int ch;
 
-    while (!Thread.interrupted() && (ch = reader.read()) != -1) {
-
-      try {
+    try {
+      while (!Thread.interrupted() && (ch = reader.read()) != -1) {
 
         String seq = buffer.append(ch).toString();
 
-        if (seq.equals(Esc.KEY_TAB.sequence())) {
-          printChar(Esc.KEY_TAB);
-        } else if (seq.equals(Esc.KEY_ENTER.sequence())) {
-          printChar(Esc.KEY_ENTER);
-        } else if (seq.equals(Esc.KEY_BACKSPACE.sequence())) {
-          printChar(Esc.KEY_BACKSPACE);
-        } else if (seq.equals(Esc.KEY_F1.sequence())) {
-          printChar(Esc.KEY_F1);
-        } else if (seq.equals(Esc.KEY_F2.sequence())) {
-          printChar(Esc.KEY_F2);
-        } else if (seq.equals(Esc.KEY_F3.sequence())) {
-          printChar(Esc.KEY_F3);
-        } else if (seq.equals(Esc.KEY_F4.sequence())) {
-          printChar(Esc.KEY_F4);
-        } else if (seq.equals(Esc.KEY_F5.sequence())) {
-          printChar(Esc.KEY_F5);
-        } else if (seq.equals(Esc.KEY_F6.sequence())) {
-          printChar(Esc.KEY_F6);
-        } else if (seq.equals(Esc.KEY_F7.sequence())) {
-          printChar(Esc.KEY_F7);
-        } else if (seq.equals(Esc.KEY_F8.sequence())) {
-          printChar(Esc.KEY_F8);
-        } else if (seq.equals(Esc.KEY_F9.sequence())) {
-          printChar(Esc.KEY_F9);
-        } else if (seq.equals(Esc.KEY_INSERT.sequence())) {
-          printChar(Esc.KEY_INSERT);
-        } else if (seq.equals(Esc.KEY_DELETE.sequence())) {
-          printChar(Esc.KEY_DELETE);
-        } else if (seq.equals(Esc.KEY_UP.sequence())) {
-          printChar(Esc.KEY_UP);
-        } else if (seq.equals(Esc.KEY_DOWN.sequence())) {
-          printChar(Esc.KEY_DOWN);
-        } else if (seq.equals(Esc.KEY_RIGHT.sequence())) {
-          printChar(Esc.KEY_RIGHT);
-        } else if (seq.equals(Esc.KEY_LEFT.sequence())) {
-          printChar(Esc.KEY_LEFT);
-        } else if (!seq.startsWith(Esc.KEY_ESC.sequence())) {
-          System.out.print((char) Integer.parseInt(buffer.toString()));
-          buffer = new StringBuilder(10);
-        }
-      } catch (UndefinedKeysException e) {
-        getLogger().error(e.getMessage());
-        kill();
+        findKey(seq);
       }
+    } catch (IOException e) {
+      kill();
+    }
+
+  }
+
+  private void findKey(String seq) {
+
+    if (seq.equals(Esc.KEY_TAB.sequence())) {
+      processKey(Esc.KEY_TAB.name());
+    } else if (seq.equals(Esc.KEY_ENTER.sequence())) {
+      processKey(Esc.KEY_ENTER.name());
+    } else if (seq.equals(Esc.KEY_BACKSPACE.sequence())) {
+      processKey(Esc.KEY_BACKSPACE.name());
+    } else if (seq.equals(Esc.KEY_F1.sequence())) {
+      processKey(Esc.KEY_F1.name());
+    } else if (seq.equals(Esc.KEY_F2.sequence())) {
+      processKey(Esc.KEY_F2.name());
+    } else if (seq.equals(Esc.KEY_F3.sequence())) {
+      processKey(Esc.KEY_F3.name());
+    } else if (seq.equals(Esc.KEY_F4.sequence())) {
+      processKey(Esc.KEY_F4.name());
+    } else if (seq.equals(Esc.KEY_F5.sequence())) {
+      processKey(Esc.KEY_F5.name());
+    } else if (seq.equals(Esc.KEY_F6.sequence())) {
+      processKey(Esc.KEY_F6.name());
+    } else if (seq.equals(Esc.KEY_F7.sequence())) {
+      processKey(Esc.KEY_F7.name());
+    } else if (seq.equals(Esc.KEY_F8.sequence())) {
+      processKey(Esc.KEY_F8.name());
+    } else if (seq.equals(Esc.KEY_F9.sequence())) {
+      processKey(Esc.KEY_F9.name());
+    } else if (seq.equals(Esc.KEY_INSERT.sequence())) {
+      processKey(Esc.KEY_INSERT.name());
+    } else if (seq.equals(Esc.KEY_DELETE.sequence())) {
+      processKey(Esc.KEY_DELETE.name());
+    } else if (seq.equals(Esc.KEY_UP.sequence())) {
+      processKey(Esc.KEY_UP.name());
+    } else if (seq.equals(Esc.KEY_DOWN.sequence())) {
+      processKey(Esc.KEY_DOWN.name());
+    } else if (seq.equals(Esc.KEY_RIGHT.sequence())) {
+      processKey(Esc.KEY_RIGHT.name());
+    } else if (seq.equals(Esc.KEY_LEFT.sequence())) {
+      processKey(Esc.KEY_LEFT.name());
+    } else if (!seq.startsWith(Esc.KEY_ESC.sequence())) {
+      processKey(String.valueOf((char) Integer.parseInt(seq)));
+      buffer = new StringBuilder(10);
     }
   }
 
@@ -99,7 +93,7 @@ public class Shell implements Runnable {
     Thread.currentThread().interrupt();
   }
 
-  private void printChar(Esc key) {
+  private void processKey(final String key) {
     System.out.print(key);
     buffer = new StringBuilder(10);
   }
